3c911026addfad38883ac78a0f55a3b8ed898b97
AidiRi <rivera.aidi@gmail.com>
2019-05-08 19:52:52 -0700

    My Select

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,17 @@
 def my_select(collection)
  # your code here!
+ if block_given?
+		new_array = []
+		i = 0
+		while i < collection.length
+			yield(collection[i])
+			if yield(collection[i]) == true
+				new_array << collection[i]
+			end
+			i = i + 1
+		end
+		new_array
+	else
+		nil
+	end
 end
